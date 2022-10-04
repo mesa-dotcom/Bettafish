2ba42422832d309f096a1750fd7ce7defd9e93de
ctith <ctith@IT-CHHOR>
2022-10-04 09:29:29 +0700

    feat: init project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblStore = new System.Windows.Forms.Label();
+            this.tbStore = new System.Windows.Forms.TextBox();
+            this.lbDebug = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCheck);
+            this.groupBox1.Controls.Add(this.lblStore);
+            this.groupBox1.Controls.Add(this.tbStore);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(518, 90);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Program Background";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(239, 35);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(103, 33);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblStore
+            // 
+            this.lblStore.AutoSize = true;
+            this.lblStore.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStore.Location = new System.Drawing.Point(6, 40);
+            this.lblStore.Name = "lblStore";
+            this.lblStore.Size = new System.Drawing.Size(68, 19);
+            this.lblStore.TabIndex = 1;
+            this.lblStore.Text = "Store Id:";
+            // 
+            // tbStore
+            // 
+            this.tbStore.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbStore.Location = new System.Drawing.Point(92, 37);
+            this.tbStore.MaxLength = 5;
+            this.tbStore.Name = "tbStore";
+            this.tbStore.Size = new System.Drawing.Size(125, 27);
+            this.tbStore.TabIndex = 0;
+            // 
+            // lbDebug
+            // 
+            this.lbDebug.FormattingEnabled = true;
+            this.lbDebug.ItemHeight = 16;
+            this.lbDebug.Location = new System.Drawing.Point(12, 108);
+            this.lbDebug.Name = "lbDebug";
+            this.lbDebug.Size = new System.Drawing.Size(518, 340);
+            this.lbDebug.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 450);
+            this.ClientSize = new System.Drawing.Size(542, 459);
+            this.Controls.Add(this.lbDebug);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bettafish";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbStore;
+        private System.Windows.Forms.Label lblStore;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.ListBox lbDebug;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net.NetworkInformation;
 
 namespace Bettafish
 {
     public partial class Form1 : Form
     {
+        Ping pinger = new Ping();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lbDebug.Items.Clear();
+            try
+            {
+                if (tbStore.Text.Length != 5)
+                {
+                    throw new Exception("Store Id is 5 digit.");
+                }
+                tbStore.Enabled = false;
+                btnCheck.Enabled = false;
+                await checking(tbStore.Text);
+            }
+            catch (Exception exc)
+            {
+                tbStore.Enabled = true;
+                btnCheck.Enabled = true;
+                MessageBox.Show(exc.Message, "Error occurs.");
+            }
+        }
+
+        private async Task checking(string storeId)
+        {
+            AddListItemText("<==============Program Start===========>");
+            string scIP = $"11{storeId[0]}.11";
+            // check network of sc
+            bool networkStatus = await PingIp(scIP);
+            if (!networkStatus)
+            {
+                throw new Exception($"SC of {storeId} ping fails.");
+            }
+            AddListItemText("Succeed ping to store 30002.");
+            // connect to database sc
+
+            // check one click files
+            // run script transaction
+            // run script promotion
+            // run script for exchange rate
+            // run script for exchange rate pos back
+            // check network of pos
+            // connect to database pos
+            // run script for exchange rate pos front
+            tbStore.Enabled = true;
+            btnCheck.Enabled = true;
+        }
+
+        private void AddListItemText(string text)
+        {
+            lbDebug.Items.Add($"{DateTime.Now:dd-MMM-yyyy}|{text}");
+        }
+
+        private async Task<bool> PingIp(string ip, int index = 0)
+        {
+            List<string> results = new List<string>();
+            PingReply prl = await pinger.SendPingAsync(ip);
+            results.Add(prl.Status.ToString());
+            return prl.Status.ToString() == "Success";
+        }
+
+        static void executeQuery(string query, SqlConnection conn)
+        {
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        static string getConnection(string source, string db_name)
+        {
+            return $"Data Source={source};Initial Catalog=master;Integrated Security=true";
         }
     }
 }
